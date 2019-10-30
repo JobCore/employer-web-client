ca4e1de9201a166aaa68786d47584eccb3e032bd
Angel Lacret <alacret@gmail.com>
2019-10-30 18:11:15 -0400

    minimium

--- webpack.common.js
+++ webpack.common.js
@@ -4,79 +4,80 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: [
-    './src/js/index.js',  
+    './src/js/index.js',
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
-    publicPath: path.resolve(__dirname, '/')
+    publicPath: path.resolve(__dirname, path.sep)
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader', 'eslint-loader']
-        },
-        { test: /\.md$/, use: [
-              {
-                  loader: "html-loader"
-              },
-              {
-                  loader: "markdown-loader",
-                  options: {
-                      /* your options here */
-                  }
-              }
-          ]
-        },
-        {
-          test: /\.scss|css$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }, {
-              loader: "sass-loader" // compiles Sass to CSS
-          }]
-        }, //css only files
-        { 
-          test: /\.(png|jpg|gif)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' } 
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.md$/, use: [
+          {
+            loader: "html-loader"
+          },
+          {
+            loader: "markdown-loader",
+            options: {
+              /* your options here */
+            }
           }
-        }, //for images
-        {
-            test: /\.svg$/,
-            loader: 'svg-inline-loader'
-        },
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)/, use: ['file-loader'] } //for fonts
+        ]
+      },
+      {
+        test: /\.scss|css$/, use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }, //css only files
+      {
+        test: /\.(png|jpg|gif)$/, use: {
+          loader: 'file-loader',
+          options: {name: '[name].[ext]'}
+        }
+      }, //for images
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)/, use: ['file-loader']} //for fonts
     ]
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
   optimization: {
-      splitChunks: {
-          cacheGroups: {
-              vendor: {
-                  test: /node_modules/, // you may add "vendor.js" here if you want to
-                  name: "vendor",
-                  chunks: "initial",
-                  enforce: true
-              }
-          }
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/, // you may add "vendor.js" here if you want to
+          name: "vendor",
+          chunks: "initial",
+          enforce: true
+        }
       }
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-        favicon: 'jobcore.ico',
-        template: 'template.html'
+      favicon: 'jobcore.ico',
+      template: 'template.html'
     })
   ]
 };
