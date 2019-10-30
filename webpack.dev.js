5101e1723e011d3a79277cd8f1a881546af0b084
Angel Lacret <alacret@gmail.com>
2019-10-30 18:15:27 -0400

    bar

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,18 @@
 const Dotenv = require('dotenv-webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: "source-map",
   devServer: {
-    contentBase:  './dist',
+    contentBase: '.' + path.sep + 'dist',
     hot: true,
     disableHostCheck: true,
     historyApiFallback: true
   },
   plugins: [
-    new Dotenv({ path: './.env.dev'})
+    new Dotenv({path: '.' + path.sep + '.env.dev'})
   ]
 });
