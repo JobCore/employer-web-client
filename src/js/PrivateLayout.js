6009669deb64788c9cccbe364fdf48954a3c2bc0
Ixax Tavarez <ixaxtav@gmail.com>
2020-01-10 17:20:19 -0500

    built page for the payroll rating

--- src/js/PrivateLayout.js
+++ src/js/PrivateLayout.js
@@ -16,7 +16,7 @@ import { Talent, ShiftInvite, ManageTalents, FilterTalents, getTalentInitialFilt
 import { PendingInvites, PendingJobcoreInvites, SearchShiftToInviteTalent, InviteTalentToJobcore, SearchTalentToInviteToShift } from './views/invites.js';
 import { ManageFavorites, AddFavlistsToTalent, FavlistEmployees, AddTalentToFavlist, Favlist, AddorUpdateFavlist } from './views/favorites.js';
 import { ManageLocations, AddOrEditLocation, Location } from './views/locations.js';
-import { ManagePayroll, PayrollReport, SelectTimesheet, EditOrAddExpiredShift } from './views/payroll.js';
+import { ManagePayroll, PayrollReport, PayrollRating, SelectTimesheet, EditOrAddExpiredShift } from './views/payroll.js';
 import { ManageRating, Rating, RatingDetails, ReviewTalent } from './views/ratings.js';
 import { Profile, PayrollSettings, ManageUsers, InviteUserToCompanyJobcore } from './views/profile.js';
 import { NOW } from './components/utils.js';
@@ -365,6 +365,7 @@ class PrivateLayout extends Flux.DashView {
                             <Route exact path='/payroll' component={ManagePayroll} />
                             <Route exact path='/payroll/period/:period_id' component={ManagePayroll} />
                             <Route exact path='/payroll/report/:period_id' component={PayrollReport} />
+                            <Route exact path='/payroll/rating/:period_id' component={PayrollRating} />
                             <Route exact path='/rate' component={RateShift} />
                             <Route exact path='/ratings' component={ManageRating} />
                             <Route exact path='/home' component={Dashboard} />
